797ef0f694b4bfc75380df5c9b2c7bd3c279de64
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-07 00:36:25 -0500

    Exercise 1

--- tuple_utils.py
+++ tuple_utils.py
@@ -14,7 +14,7 @@ def tic_tac_toe_finish(board: TicTacToeBoard, pos_y: int, pos_x: int, symbol: st
     :param symbol: The symbol that should be placed in the column (X, or O)
     :return: None
     """
-    pass  # remove pass statement and implement me
+    board[pos_y][pos_x] = symbol
 
 
 def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
@@ -25,7 +25,11 @@ def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
     :param instance: An item in the collection parameter
     :return: An integer.
     """
-    pass  # remove pass statement and implement me
+    total = 0
+    for i in collection:
+        if i == instance:
+            total += 1
+    return total
 
 
 def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
@@ -38,7 +42,10 @@ def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
     :param entries: A list or tuple
     :return: None
     """
-    pass  # remove pass statement and implement me
+    for i,j in zip(indexes, entries):
+        a = 'Index: ' + str(i)
+        b = 'Entry: ' + str(j)
+        print(a + ' '*(11 - len(str(i))) + b)
 
 
 def print_items_with_index(items: Iterable):
@@ -50,5 +57,6 @@ def print_items_with_index(items: Iterable):
     :param items: A tuple or a list
     :return: None
     """
-    pass  # remove pass statement and implement me
+    for index, element in enumerate(items, start = 1):
+        print(str(index)+': '+element)
 
